334327ef35a3e056c8f08bf82d226599fb258f5d
NTE ADMIN <mawayalebo@gmail.com>
2022-06-19 11:19:00 +0200

    inserting favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center py-2">
+    <section className="">
       <Head>
         <title>Lebo Mini Profile | the Next JS Rockstar</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       hi its lebo here
 
